a4a8b288a5ce92cace12f83fff2d527db263243a
Ahmet Artu Yildirim <ahmet@artulab.com>
2022-12-25 01:59:56 -0800

    Generate pkg-config file

--- build-debug.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./configure CPPFLAGS=-DDEBUG CFLAGS="-g -O0" && make
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf aclocal.m4 config.sub configure depcomp m4 ar-lib compile config.guess config.h install-sh test-driver Makefile Makefile.in autom4te.cache config.h.in ltmain.sh missing libtool config.status config.log INSTALL stamp-h1 src/Makefile.in src/.libs src/.deps src/*.o src/Makefile src/libprogbuf.la src/progbuf.lo src/check_progbuf build config.h.in~
+rm -rf aclocal.m4 config.sub configure depcomp m4 ar-lib compile config.guess config.h install-sh test-driver Makefile Makefile.in autom4te.cache config.h.in ltmain.sh missing libtool config.status config.log INSTALL stamp-h1 src/Makefile.in src/.libs src/.deps src/*.o src/Makefile src/libprogbuf.la src/progbuf.lo src/check_progbuf src/test-suite.log src/libprogbuf_la-progbuf.lo src/check_progbuf.log src/check_progbuf.trs src/libprogbuf.pc build config.h.in~
